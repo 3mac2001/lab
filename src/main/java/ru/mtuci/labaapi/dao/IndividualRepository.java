5fb8b4215245350897ed2a41b8edcc0552e81872
3mac2001 <74017851+3mac2001@users.noreply.github.com>
2020-11-10 17:27:16 +0300

    Update IndividualRepository.java

--- src/main/java/ru/mtuci/labaapi/dao/IndividualRepository.java
+++ src/main/java/ru/mtuci/labaapi/dao/IndividualRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ru.mtuci.apilab.model.Individual;
+import ru.mtuci.labaapi.model.Individual;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
