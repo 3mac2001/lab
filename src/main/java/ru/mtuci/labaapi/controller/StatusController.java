567157a6e388bf5b52f93e4f9d9a7c4f966a0aba
3mac2001 <74017851+3mac2001@users.noreply.github.com>
2020-11-10 17:29:08 +0300

    Update StatusController.java

--- src/main/java/ru/mtuci/labaapi/controller/StatusController.java
+++ src/main/java/ru/mtuci/labaapi/controller/StatusController.java
@@ -5,7 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.mtuci.apilab.dto.Status;
+import ru.mtuci.labaapi.dto.Status;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
